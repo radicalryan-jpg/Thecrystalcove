21146c1e7f7f83cc8a1c19d35583188404faa870
radicalryan-jpg <ryanwestcott1111@gmail.com>
2025-09-06 17:33:37 -0400

    Delete src directory

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import StartScreen from "./StartScreen";
-import bg from "./assets/the-crystal-cove.png";
-
-export default function App() {
-  return <StartScreen bg={bg} onStart={() => console.log("Game started")} />;
-}
